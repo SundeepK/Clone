328f336cab9208f33c070df746ad45067798c16f
Sundeep <sundeepkahlon@hotmail.com>
2014-05-05 21:56:31 +0100

    init project

---
+++ Clone/src/main.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antialiasingLevel = 4;
+    settings.majorVersion = 3;
+    settings.minorVersion = 0;
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    // Create the main window
+    sf::RenderWindow App(sf::VideoMode(1280, 800), "SFML window", settings);
+
+
+	// Start the game loop
+    while (App.isOpen())
+    {
+        sf::Event Event;
+        while (App.pollEvent(Event))
+        {
+            if (Event.type == sf::Event::Closed)
+            {
+                App.close();
+            }
+        }
+
+        // Clear screen
+        App.clear();
+
+        // Update the window
+        App.display();
+    }
+
+    return EXIT_SUCCESS;
+}
