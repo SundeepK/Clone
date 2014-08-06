762648ebdc3c24fb563ca39bef8ace1e8b0b9c44
Sundeep <sundeepkahlon@hotmail.com>
2014-08-06 22:57:15 +0100

    Added basic component classes

--- src/components/PlayerControlsComponent.h
+++ src/components/PlayerControlsComponent.h
@@ -1,8 +1,19 @@
 #ifndef PLAYERCONTROLSCOMPONENT_H_
 #define PLAYERCONTROLSCOMPONENT_H_
 
+#include <anax/Component.hpp>
 
+class PlayerControlsComponent : anax::Component<PlayerControlsComponent>
+{
+	public:
+		enum class PlayerMovementControls {
+			NO_STATE = -1,
+			DEFAULT_STATE = 0,
+			MOVE_LEFT,
+			MOVE_RIGHT,
+			JUMP
+	};
 
-
+};
 
 #endif /* PLAYERCONTROLSCOMPONENT_H_ */
--- src/components/PlayerStateComponenet.h
+++ src/components/PlayerStateComponenet.h
@@ -1,7 +1,22 @@
 #ifndef PLAYERSTATECOMPONENET_H_
 #define PLAYERSTATECOMPONENET_H_
 
+class PlayerStateComponent : anax::Component<PlayerStateComponent>
+{
+	public:
+		enum class State {
+		NO_STATE = -1,
+		DEFAULT_STATE = 0,
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		JUMP
+	};
 
+	private:
+		State m_state;
+
+
+};
 
 
 
