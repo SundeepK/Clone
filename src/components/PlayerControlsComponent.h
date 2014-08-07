7b74d2883194dce811528195bc0293478f27c069
Sundeep <sundeepkahlon@hotmail.com>
2014-08-07 23:25:34 +0100

    Added controls component

--- src/components/PlayerControlsComponent.h
+++ src/components/PlayerControlsComponent.h
@@ -2,18 +2,23 @@
 #define PLAYERCONTROLSCOMPONENT_H_
 
 #include <anax/Component.hpp>
+#include <SFML/System.hpp>
 
 class PlayerControlsComponent : anax::Component<PlayerControlsComponent>
 {
 	public:
-		enum class PlayerMovementControls {
-			NO_STATE = -1,
-			DEFAULT_STATE = 0,
-			MOVE_LEFT,
-			MOVE_RIGHT,
-			JUMP
-	};
+	 struct Controls
+	    {
+	       typedef sf::Keyboard::Key Key;
 
+	       const Key JUMP_KEY = Key::W;
+	       const Key LEFT_KEY = Key::A;
+	       const Key DOWN_KEY = Key::S;
+	       const Key RIGHT_KEY = Key::D;
+
+	    };
+
+	 const Controls controls;
 };
 
 #endif /* PLAYERCONTROLSCOMPONENT_H_ */
